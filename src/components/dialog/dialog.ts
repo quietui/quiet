08a8be87f9eff4993a90fd778314fc69bcb58a21
Cory LaViska <cory@abeautifulsite.net>
2024-03-03 08:11:40 -0500

    use icon

--- src/components/dialog/dialog.ts
+++ src/components/dialog/dialog.ts
@@ -155,15 +155,7 @@ export class Dialog extends QuietElement {
                 <slot name="header"></slot>
                 <slot name="actions" class="actions">
                   <quiet-button slot="header" variant="text" icon-label="Close" data-dialog="dismiss">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                    >
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
+                    <quiet-icon name="x-mark"></quiet-icon>
                   </quiet-button>
                 </slot>
               </header>

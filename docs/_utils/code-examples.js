7c8272fdf7c33e0b28812e4102751c51e2b0dc2b
Cory LaViska <cory@abeautifulsite.net>
2024-03-04 20:40:52 -0500

    use icons

--- docs/_utils/code-examples.js
+++ docs/_utils/code-examples.js
@@ -48,9 +48,7 @@ export function codeExamplesPlugin(options = {}) {
                 aria-controls="${id}"
               >
                 Code
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
-                  <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-                </svg>
+                <quiet-icon name="chevron-down" family="micro"></quiet-icon>
               </button>
 
               ${
@@ -59,10 +57,7 @@ export function codeExamplesPlugin(options = {}) {
                   : `
                     <button class="code-example-pen" type="button">
                       Edit
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
-                        <path d="M6.22 8.72a.75.75 0 0 0 1.06 1.06l5.22-5.22v1.69a.75.75 0 0 0 1.5 0v-3.5a.75.75 0 0 0-.75-.75h-3.5a.75.75 0 0 0 0 1.5h1.69L6.22 8.72Z" />
-                        <path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" />
-                      </svg>
+                      <quiet-icon name="arrow-top-right-on-square" family="micro"></quiet-icon>
                     </button>
                   `
               }

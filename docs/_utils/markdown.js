04aa1a1279bc624e7648e39c30bd14be284ce6ad
Cory LaViska <cory@abeautifulsite.net>
2024-03-09 19:27:46 -0500

    update icon

--- docs/_utils/markdown.js
+++ docs/_utils/markdown.js
@@ -25,7 +25,7 @@ markdown.use(markdownItMark);
   markdown.use(markdownItContainer, type, {
     render: function (tokens, idx) {
       const variant = type === 'danger' ? 'destructive' : 'primary';
-      const icon = type === 'danger' ? 'hand-raised' : 'cursor-arrow-rays';
+      const icon = type === 'danger' ? 'hand-raised' : 'light-bulb';
       if (tokens[idx].nesting === 1) {
         return `
           <quiet-callout variant="${variant}" with-icon>

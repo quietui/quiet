62952340c6eea68f9886992f2a0d86799fe5d99b
Cory LaViska <cory@abeautifulsite.net>
2024-06-13 20:01:40 -0400

    only log analytics for the public domain

--- docs/_utils/analytics.js
+++ docs/_utils/analytics.js
@@ -9,10 +9,19 @@ export function analyticsPlugin(options = {}) {
 
   return function (eleventyConfig) {
     eleventyConfig.addTransform('analytics', function (content) {
+      // Snippet from https://plausible.io/docs/script-extensions
       return content.replace(
         '</head>',
         `
-          <script defer data-domain="${options.domain}" src="https://plausible.io/js/script.js"></script>
+            <script defer data-domain="${options.domain}" src="https://plausible.io/js/script.manual.js"></script>
+            <script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>
+            <script>
+              document.addEventListener('turbo:load', function() {
+                if (location.hostname === '${options.domain}') {
+                  plausible('pageview');
+                }
+              });
+            </script>
           </head>
         `
       );

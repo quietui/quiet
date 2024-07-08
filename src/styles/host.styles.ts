3616a185b7d3b2517c92f72b0d1ef82ce21af10b
Cory LaViska <cory@abeautifulsite.net>
2024-07-08 10:38:58 -0400

    ensure box sizing for components

--- src/styles/host.styles.ts
+++ src/styles/host.styles.ts
@@ -3,13 +3,13 @@ import { css } from 'lit';
 /** Styles that should get applied to every component. */
 export default css`
   :host {
-    box-sizing: border-box;
+    box-sizing: border-box !important;
   }
 
   :host *,
   :host *::before,
   :host *::after {
-    box-sizing: border-box;
+    box-sizing: border-box !important;
   }
 
   [hidden] {

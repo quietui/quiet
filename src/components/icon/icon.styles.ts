45eb577202138aa5a2f679e567cd1dafcc3c81b0
Cory LaViska <cory@abeautifulsite.net>
2024-07-09 09:36:23 -0400

    allow icons to have padding

--- src/components/icon/icon.styles.ts
+++ src/components/icon/icon.styles.ts
@@ -3,15 +3,13 @@ import { css } from 'lit';
 export default css`
   :host {
     display: inline-block;
-    width: auto;
-    height: 1em;
     stroke-width: 1.5px;
   }
 
   svg {
     display: block;
     width: auto;
-    height: 100%;
+    height: 1em;
     stroke-width: inherit;
   }
 `;

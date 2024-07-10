4c2a734bc734ac9824a5e922e0141d86a21eada7
Cory LaViska <cory@abeautifulsite.net>
2024-07-09 21:59:17 -0400

    revert icon styles

--- src/components/icon/icon.styles.ts
+++ src/components/icon/icon.styles.ts
@@ -4,12 +4,14 @@ export default css`
   :host {
     display: inline-block;
     stroke-width: 1.5px;
+    width: auto;
+    height: 1em;
   }
 
   svg {
     display: block;
     width: auto;
-    height: 1em;
+    height: 100%;
     stroke-width: inherit;
   }
 `;

b09e3182f97f0aaf12c2019874848d85d66a37bc
Cory LaViska <cory@abeautifulsite.net>
2024-07-16 16:55:22 -0400

    fix border radius

--- src/components/file-input/file-input.styles.ts
+++ src/components/file-input/file-input.styles.ts
@@ -89,7 +89,6 @@ export default css`
       width: 100%;
       aspect-ratio: 1;
       background: var(--quiet-neutral-fill-softer);
-      border-radius: var(--quiet-border-radius);
       border-radius: calc(var(--quiet-border-radius) * 0.5);
 
       img {
@@ -97,6 +96,7 @@ export default css`
         width: 100%;
         height: 100%;
         object-fit: cover;
+        border-radius: inherit;
       }
 
       quiet-icon {

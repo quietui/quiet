24996a88cd90dddd389eb52ab4ccf9a5d2e4b722
Cory LaViska <cory@abeautifulsite.net>
2024-08-09 17:54:07 -0400

    add star

--- src/utilities/icon-library.ts
+++ src/utilities/icon-library.ts
@@ -160,6 +160,12 @@ registerIconLibrary('system', {
       `);
     }
 
+    if (name === 'star' && family === 'outline') {
+      return encode(`
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
+      `);
+    }
+
     if (name === 'upload' && family === 'outline') {
       return encode(`
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 9l5 -5l5 5" /><path d="M12 4l0 12" /></svg>

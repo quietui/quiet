4648e8143b55ae7858616342f1b04292c312ca7d
Cory LaViska <cory@abeautifulsite.net>
2025-01-05 21:09:04 -0500

    improve styles

--- src/components/keyboard-shortcut/keyboard-shortcut.styles.ts
+++ src/components/keyboard-shortcut/keyboard-shortcut.styles.ts
@@ -2,17 +2,17 @@ import { css } from 'lit';
 
 export default css`
   :host([appearance='normal']) {
-    display: inline;
-    padding: 0.125em 0.33em;
+    display: inline-block;
+    padding-inline: 0.33em;
     border: solid max(1px, 0.0715em) var(--quiet-neutral-fill-soft);
     border-radius: calc(var(--quiet-border-radius) / 2);
     box-shadow: 0 max(1px, 0.0715em) 0 0 var(--quiet-neutral-fill-soft);
+    vertical-align: baseline;
     white-space: nowrap;
   }
 
   kbd {
     margin-inline: 0.0625em;
-    font: inherit;
 
     &:first-of-type {
       margin-inline-start: 0;

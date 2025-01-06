11e49349f9461be08105c802ca4a6374328d872e
Cory LaViska <cory@abeautifulsite.net>
2025-01-06 08:51:50 -0500

    fix selection color when highlighted

--- src/components/keyboard-shortcut/keyboard-shortcut.styles.ts
+++ src/components/keyboard-shortcut/keyboard-shortcut.styles.ts
@@ -22,5 +22,10 @@ export default css`
     &:last-of-type {
       margin-inline-end: 0;
     }
+
+    &::selection {
+      background-color: var(--quiet-selection-background-color);
+      color: var(--quiet-selection-color);
+    }
   }
 `;

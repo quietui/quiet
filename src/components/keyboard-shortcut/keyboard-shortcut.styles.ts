b4845f61eb02ecda5906ca6712a252f609c57de1
Cory LaViska <cory@abeautifulsite.net>
2025-01-06 08:54:04 -0500

    add min width

--- src/components/keyboard-shortcut/keyboard-shortcut.styles.ts
+++ src/components/keyboard-shortcut/keyboard-shortcut.styles.ts
@@ -3,10 +3,12 @@ import { css } from 'lit';
 export default css`
   :host([appearance='normal']) {
     display: inline-block;
+    min-width: 1.25em;
     padding-inline: 0.33em;
     border: solid max(1px, 0.0715em) var(--quiet-neutral-fill-soft);
     border-radius: calc(var(--quiet-border-radius) / 2);
     box-shadow: 0 max(1px, 0.0715em) 0 0 var(--quiet-neutral-fill-soft);
+    text-align: center;
     vertical-align: baseline;
     white-space: nowrap;
   }

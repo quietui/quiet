84c5e92bd092b4fb8563583103b003f1b80ff9be
Cory LaViska <cory@abeautifulsite.net>
2025-01-25 12:54:39 -0500

    load all components in the test runner

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -2,12 +2,21 @@ import { esbuildPlugin } from '@web/dev-server-esbuild';
 import { playwrightLauncher } from '@web/test-runner-playwright';
 import { globbySync } from 'globby';
 import os from 'os';
+import { getComponents } from './docs/_utils/manifest.js';
 
 // Only run one browser per core
 const cores = os.availableParallelism?.() ?? os.cpus.length;
 const browsers = ['chromium', 'firefox', 'webkit'];
 const concurrentBrowsers = Math.min(browsers.length, cores);
 const concurrency = Math.max(Math.floor(cores / browsers.length), 1);
+
+// Import all components up front so we don't have to wait for the autoloader in every test
+const componentImports = getComponents()
+  .map(component => {
+    const baseName = component.tagName.replace(/^quiet-/, '');
+    return `import '/dist/components/${baseName}/${baseName}.js';`;
+  })
+  .join('\n');
 
 // https://modern-web.dev/docs/test-runner/cli-and-configuration/
 export default {
@@ -37,7 +46,9 @@ export default {
 <html lang="en-US">
   <head></head>
   <body>
-    <link rel="stylesheet" href="dist/quiet.css">
+    <link rel="stylesheet" href="/dist/themes/quiet.css">
+    <link rel="stylesheet" href="/dist/themes/restyle.css">
+    <script type="module">${componentImports}</script>
     <script>
       window.process = {env: { NODE_ENV: "production" }}
     </script>

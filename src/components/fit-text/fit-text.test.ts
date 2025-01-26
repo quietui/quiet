e99c1cf5acb8b1df9121da68983f15284189d3b4
Cory LaViska <cory@abeautifulsite.net>
2025-01-26 10:24:54 -0500

    unskip

--- src/components/fit-text/fit-text.test.ts
+++ src/components/fit-text/fit-text.test.ts
@@ -2,7 +2,7 @@ import { expect, fixture, html } from '@open-wc/testing';
 import type { QuietFitText } from './fit-text.js';
 
 describe('<quiet-fit-text>', () => {
-  it.skip('does something', async () => {
+  it('does something', async () => {
     const el = await fixture<QuietFitText>(html` <quiet-fit-text>Click me</quiet-fit-text> `);
     expect(customElements.get(el.localName)).to.not.be.undefined;
   });

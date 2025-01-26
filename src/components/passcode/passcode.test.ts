d5c295056550b13b800e6240636c1f2bf670e77c
Cory LaViska <cory@abeautifulsite.net>
2025-01-25 20:04:57 -0500

    passcode event tests

--- src/components/passcode/passcode.test.ts
+++ src/components/passcode/passcode.test.ts
@@ -1,9 +1,62 @@
 import { expect, fixture, html } from '@open-wc/testing';
+import { sendKeys } from '@web/test-runner-commands';
+import { spy } from 'sinon';
 import type { QuietPasscode } from './passcode.js';
 
 describe('<quiet-passcode>', () => {
-  it('does something', async () => {
-    const el = await fixture<QuietPasscode>(html` <quiet-passcode>Click me</quiet-passcode> `);
-    expect(customElements.get(el.localName)).to.not.be.undefined;
+  it('should dispatch the `quiet-input` and `input` events when the user enters a value', async () => {
+    const el = await fixture<QuietPasscode>(html`<quiet-passcode label="Test"></quiet-passcode>`);
+
+    const inputSpy = spy();
+    const quietInputSpy = spy();
+
+    el.addEventListener('input', inputSpy);
+    el.addEventListener('quiet-input', quietInputSpy);
+
+    el.focus();
+    await el.updateComplete;
+    await sendKeys({ type: '123' });
+    await el.updateComplete;
+    el.blur();
+
+    // Custom event
+    expect(quietInputSpy).to.have.been.calledThrice;
+    expect(inputSpy).to.have.been.calledThrice;
+  });
+
+  it('should dispatch the `quiet-change` and `change` events when the user commits a value', async () => {
+    const el = await fixture<QuietPasscode>(html`<quiet-passcode label="Test"></quiet-passcode>`);
+
+    const quietChangeSpy = spy();
+    const changeSpy = spy();
+
+    el.addEventListener('change', changeSpy);
+    el.addEventListener('quiet-change', quietChangeSpy);
+
+    el.focus();
+    await el.updateComplete;
+    await sendKeys({ type: '123' });
+    await el.updateComplete;
+    el.blur();
+
+    // Custom event
+    expect(quietChangeSpy).to.have.been.calledOnce;
+    expect(changeSpy).to.have.been.calledOnce;
+  });
+
+  it('should dispatch the `quiet-blur` and `quiet-change` events when entering and leaving the control', async () => {
+    const el = await fixture<QuietPasscode>(html`<quiet-passcode label="Test"></quiet-passcode>`);
+
+    const focusSpy = spy();
+    const blurSpy = spy();
+
+    el.addEventListener('quiet-focus', focusSpy);
+    el.addEventListener('quiet-blur', blurSpy);
+
+    el.focus();
+    el.blur();
+
+    expect(focusSpy).to.have.been.calledOnce;
+    expect(blurSpy).to.have.been.calledOnce;
   });
 });

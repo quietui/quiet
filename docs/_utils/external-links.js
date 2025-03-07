fce1d943ada1e998d6de9bc6cefecf221ac8827d
Cory LaViska <cory@abeautifulsite.net>
2025-03-06 20:47:50 -0500

    fuck this wrapping nonsense

--- docs/_utils/external-links.js
+++ docs/_utils/external-links.js
@@ -13,7 +13,6 @@ export function externalLinksPlugin(options = {}) {
     eleventyConfig.addTransform('external-links', function (content) {
       const doc = parse(content);
       const container = doc.querySelector(options.container);
-      const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-external-link"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" /><path d="M11 13l9 -9" /><path d="M15 4h5v5" /></svg>`;
 
       if (!container) {
         return content;
@@ -34,26 +33,9 @@ export function externalLinksPlugin(options = {}) {
           if (noIcon) {
             a.removeAttribute('data-no-external-icon');
           } else {
-            // Get the current inner HTML and split into words
-            const textContent = a.textContent.trim();
-            const words = textContent.split(/\s+/);
-
-            if (words.length > 0) {
-              // Take all words except the last one
-              const textBeforeLast = words.slice(0, -1).join(' ');
-              const lastWord = words[words.length - 1];
-
-              // Create the new HTML structure
-              a.innerHTML = `
-                ${textBeforeLast ? textBeforeLast + ' ' : ''}
-                <span>${lastWord}${svg}</span>
-              `.trim();
-            } else {
-              // If no words, just add the SVG in a nowrap span
-              a.innerHTML = `
-                <span>${svg}</span>
-              `.trim();
-            }
+            a.innerHTML += `
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-external-link"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" /><path d="M11 13l9 -9" /><path d="M15 4h5v5" /></svg>
+            `.trim();
           }
         }
       });

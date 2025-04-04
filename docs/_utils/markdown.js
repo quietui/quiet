375a158c0ef1f75eb3f5a61e3e68c2410d84c498
Cory LaViska <cory@abeautifulsite.net>
2025-04-04 08:52:37 -0400

    let's try green

--- docs/_utils/markdown.js
+++ docs/_utils/markdown.js
@@ -24,8 +24,8 @@ markdown.use(markdownItMark);
 ['info', 'warn'].forEach(type => {
   markdown.use(markdownItContainer, type, {
     render: function (tokens, idx) {
-      const variant = type === 'warn' ? 'destructive' : 'primary';
-      const icon = type === 'warn' ? 'hand-stop' : 'north-star';
+      const variant = type === 'warn' ? 'destructive' : 'constructive';
+      const icon = type === 'warn' ? 'hand-stop' : 'hand-click';
       if (tokens[idx].nesting === 1) {
         return `
           <quiet-callout variant="${variant}">

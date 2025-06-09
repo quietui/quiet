cfefc84bc6666622624e2df32c185c9df528648b
Cory LaViska <cory@abeautifulsite.net>
2025-06-09 17:23:23 -0400

    bulletproof

--- src/utilities/parse.ts
+++ src/utilities/parse.ts
@@ -1,6 +1,6 @@
 /** Parses a space-delimited set of tokens and returns an array with all whitespace removed. */
-export function parseSpaceDelimitedTokens(input: string): string[] {
-  return input
+export function parseSpaceDelimitedTokens(input: string | number): string[] {
+  return (input + '')
     .split(' ')
     .map(token => token.trim())
     .filter(token => token !== '');

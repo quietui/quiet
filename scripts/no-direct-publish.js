ed63b421100972c8e17915b579b2449b6d33d1ad
Cory LaViska <cory@abeautifulsite.net>
2025-09-20 16:26:33 -0400

    update error

--- scripts/no-direct-publish.js
+++ scripts/no-direct-publish.js
@@ -4,9 +4,7 @@ import chalk from 'chalk';
 // NOTE: the release script sets this ENV to publish both packages programmatically
 //
 if (!process.env.QUIET_UI_RELEASE) {
-  console.log(chalk.magenta('\n🐭 Please use one of the following scripts to publish:\n'));
-  console.log(chalk.gray('  • npm run release'));
-  console.log(chalk.gray('  • npm run release:dry-run\n'));
+  console.log(chalk.magenta('\n🐭 Please use the private publish script in @quietui/publish to publish releases.\n'));
 
   // Exit with error to prevent publishing
   process.exit(1);

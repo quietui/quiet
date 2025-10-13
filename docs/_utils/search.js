dc68a263b4156ce029e94b893c2ecf37e7233895
Cory LaViska <cory@abeautifulsite.net>
2025-10-13 17:20:42 -0400

    fix page name

--- docs/_utils/search.js
+++ docs/_utils/search.js
@@ -24,7 +24,7 @@ export function searchPlugin(options = {}) {
   return function (eleventyConfig) {
     const pagesToSkip = [
       '/404.html', // permalinks use the extension
-      '/thanks/' // other URLs do not
+      '/purchase-complete/' // other URLs do not
     ];
     const pagesToIndex = [
       //
